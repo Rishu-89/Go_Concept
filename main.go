85e11d3cbfd2ffbe08085157e34c03e76ab20036
Rishu Pandey <rishu.pandey@calsoftinc.com>
2026-05-11 01:01:02 +0530

    Diffrence between cap and len

--- main.go
+++ main.go
@@ -3,17 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	prices := [4]float64{10.23, 21.7}
-	prices[3]=19
-	prices[2]=12.4
-	fmt.Println(prices)
-	featuredPrices:=prices[1:3]			// including one excluding last
-	fmt.Println(featuredPrices)
-	featuredPrices=prices[1:]
-	fmt.Println(featuredPrices)
-	featuredPrices=prices[:3]
-	fmt.Println(featuredPrices)
-	featuredPrices[1]=9777
-	fmt.Println(prices)					// [10.23 9777 12.4 19] Important
+	prices := [6]float64{10.23, 21.7,5,6}
+	prices[4]=19
+	prices[5]=12.4
+	// fmt.Println(prices)
+	// featuredPrices:=prices[1:3]			// including one excluding last
+	// fmt.Println(featuredPrices)
+	// featuredPrices=prices[1:]
+	// fmt.Println(featuredPrices)
+	// featuredPrices=prices[:3]
+	// fmt.Println(featuredPrices)
+	// featuredPrices[1]=9777
+	// fmt.Println(prices)					// [10.23 9777 12.4 19] Important
 	
+	featuredPrice:=prices[2:5]
+	fmt.Println(len(featuredPrice),cap(featuredPrice))  // output 3 4
+														// slice always remembar the last not first
+
+
+
 }
